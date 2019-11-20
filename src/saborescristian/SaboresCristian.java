c9233a1dd7c76b19e070b66f9372f6ed75c7718e
Your-Full-Name <your-email-address>
2019-11-20 20:24:34 -0300

    Segunda version

---
+++ src/saborescristian/SaboresCristian.java
@@ -0,0 +1,14 @@
+
+package saborescristian;
+
+public class SaboresCristian {
+
+   
+    public static void main(String[] args) {
+        String[]sabores = {"Dulce", "Salado", "Amargo", "Acido", "Umami", "Picante", "Astrigente", "Starchy", "Adiposo"};
+        for(int i=0;i<6;i++){
+            System.out.println("Sabor:"+sabores[i]);
+        }
+    }
+    
+}
