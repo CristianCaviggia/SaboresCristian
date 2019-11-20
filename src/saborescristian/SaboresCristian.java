94acfd3e11d0471c4a57ee0f67f6acf0bc435b7c
Your-Full-Name <your-email-address>
2019-11-20 20:39:03 -0300

    Se agregaron sabores.

--- src/saborescristian/SaboresCristian.java
+++ src/saborescristian/SaboresCristian.java
@@ -5,7 +5,7 @@ public class SaboresCristian {
 
    
     public static void main(String[] args) {
-        String[]sabores = {"Dulce", "Salado", "Amargo", "Acido", "Umami", "Picante", "Astrigente", "Starchy", "Adiposo"};
+        String[]sabores = {"Dulce", "Salado", "Amargo", "Acido", "Umami", "Picante", "Astrigente", "Starchy", "Adiposo", "Chocolate"};
         for(int i=0;i<6;i++){
             System.out.println("Sabor:"+sabores[i]);
         }
